c20db0788fff2d35e81a1d3831009e9ce21b707f
Davide Primino <dprimino@olivelliputelli.it>
2019-01-08 17:11:29 +0100

    Classe Cerchio

---
+++ ClassiUtiliClassLibrary/Cerchio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassiUtiliClassLibrary
+{
+    class Cerchio
+    {
+        public Double Raggio { get; set; }
+    }
+}
