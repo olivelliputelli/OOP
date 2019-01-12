be4723ff759d6e6707085634b3c33d73aee5291b
Davide Primino <dprimino@olivelliputelli.it>
2019-01-12 08:21:56 +0100

    Aggiunto metodo Equals a Cerchio

--- ClassiUtiliClassLibrary/Cerchio.cs
+++ ClassiUtiliClassLibrary/Cerchio.cs
@@ -8,13 +8,17 @@ namespace ClassiUtiliClassLibrary
 
         public double Raggio
         {
-            get { return raggio; }
-            set { raggio = (value >= 0) ? value : throw new IndexOutOfRangeException(); }
+            get => raggio;
+            set => raggio = (value >= 0) ? value : throw new IndexOutOfRangeException();
         }
 
         public double Area() => Math.PI * Math.Pow(this.Raggio, 2);
 
         public double Circonferenza() => 2 * Math.PI * this.Raggio;
 
+        public override bool Equals(object obj)
+            => this.Raggio == (obj as Cerchio).Raggio;
+
+        public override int GetHashCode() => base.GetHashCode();
     }
 }
