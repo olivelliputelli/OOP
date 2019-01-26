09cb563b062d98d998ab1febb73f7c16437a26a2
Davide Primino <dprimino@olivelliputelli.it>
2019-01-26 08:49:27 +0100

    Classe Cerchio

---
+++ MatematicaClassLibrary/Geometria2D/Cerchio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatematicaClassLibrary.Geometria2D
+{
+    /// <summary>
+    /// Classe utile per gestire cerchi geometrici.
+    /// </summary>
+    public class Cerchio
+    {
+        private double raggio;
+
+        /// <value>
+        /// get e set del raggio del cerchio.
+        /// </value>
+        /// 
+        public double Raggio
+        {
+            get { return raggio; }
+            set { raggio = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double Area()
+        {
+            return Math.PI * Math.Pow(Raggio, 2);
+        }
+    }
+}
